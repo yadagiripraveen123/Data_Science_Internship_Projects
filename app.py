386e84321f66c114b41b98433b1db68b143a404f
yadagiripraveen123 <119128028+yadagiripraveen123@users.noreply.github.com>
2023-02-27 15:19:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.header("Hello Data Science Internship 2023!")
+st.title("Welcome to my web app")
+st.header("This is Yadagiri Praveen kumar")
+
+st.subheader("Her you Find the intersting Dashboards in this web app")
+
+button = st.button("Click Here!")
+
+if button == True:
+    st.subheader("Let's Start :cool:")
