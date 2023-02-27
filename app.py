8952b004e45be6f250872209b40a6ab624281430
yadagiripraveen123 <119128028+yadagiripraveen123@users.noreply.github.com>
2023-02-27 16:00:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Welcome to my web app")
 st.header("This is Yadagiri Praveen kumar")
 
 st.subheader("As a Data scientist I created my Streamlit app here Take a look...!")
-st.subheader("Here you can connect me through github and linkedin")
+st.subheader("You can connect me through github and linkedin")
 
 button = st.button("Click Here!")
 
