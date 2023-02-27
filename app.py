fab80c1ecf1943719a63c9d6d4f2bb88f36c0cf1
yadagiripraveen123 <119128028+yadagiripraveen123@users.noreply.github.com>
2023-02-27 15:37:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ st.title("Welcome to my web app")
 st.header("This is Yadagiri Praveen kumar")
 
 st.subheader("Her you Find the intersting Dashboards in this web app")
+st.subheader("Here you can connect me through github and linkedin")
 
 button = st.button("Click Here!")
 
 if button == True:
     st.subheader("Let's Start :cool:")
+    st.markdown("https://github.com/yadagiripraveen123")
+    st.markdown("https://www.linkedin.com/in/yadagiri-praveen-kumar-89326a1b2/")
+    
