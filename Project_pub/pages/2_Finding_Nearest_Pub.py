89a1fb900a74a38ebd44c70fd5297a7dcba55c4b
yadagiripraveen123 <119128028+yadagiripraveen123@users.noreply.github.com>
2023-04-23 13:27:14 +0530

    Add files via upload

--- Project_pub/pages/2_Finding_Nearest_Pub.py
+++ Project_pub/pages/2_Finding_Nearest_Pub.py
@@ -9,7 +9,7 @@ st.header("Search for the Nearest Pubs")
 # Load data
 FILE_DIR1 = os.path.dirname(os.path.abspath("__file__"))
 data_path = os.path.join(FILE_DIR1,"resources","data","open_pubs_cleaned.csv")
-pub_df = pd.read_csv(f'data_path')
+pub_df = pd.read_csv(r'data_path')
 
 #Take input -latitude and longitude
 col1,col2=st.columns(2)
